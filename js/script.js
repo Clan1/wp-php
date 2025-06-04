68617a24a027320ecd28c0f263e83c442847b46a
Clan1 <cl.an@iol.it>
2025-06-04 18:57:18 +0200

    wp-php
    pr

---
+++ js/script.js
@@ -0,0 +1,15 @@
+jQuery(document).ready(function($) {
+    $('.upload_image_button').click(function(e) {
+        e.preventDefault();
+
+        var button = $(this);
+        var custom_uploader = wp.media({
+            title: 'Select Image',
+            button: { text: 'Use this image' },
+            multiple: false
+        }).on('select', function() {
+            var attachment = custom_uploader.state().get('selection').first().toJSON();
+            button.prev('input').val(attachment.url);
+        }).open();
+    });
+});
